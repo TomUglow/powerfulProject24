cf78d6f97e0a951b4cba10d62607fbd27d3f671c
TomUglow <tom.uglow@hotmail.com>
2024-07-30 20:31:36 +1000

    quote section button links

--- src/Pages/components/Footer.js
+++ src/Pages/components/Footer.js
@@ -1,37 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import '../../stylesheets/Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope, faHouse } from '@fortawesome/free-solid-svg-icons';
-import FacebookLogo from '../../assets/icons/facebook_white.png'
-import InstagramLogo from '../../assets/icons/instagram_white.png'
+import FacebookLogo from '../../assets/icons/facebook_white.png';
+import InstagramLogo from '../../assets/icons/instagram_white.png';
 import FooterLogo from '../../assets/logos/final_logo_black.jpg';
 
 const Footer = () => {
   return (
     <footer className="site-footer">
       <div className="black-footer">
-      <div className="black-footer-content">
-        <div className="service-areas">
-          <h2>Service Area</h2>
-          <p>Servicing all of Melbourne including Geelong and Torquay</p>
-          <a href='/' className='footer-link'><FontAwesomeIcon icon={faHouse} className='home-link'/></a>
-        </div>
-        <div className="contact-details">
-          <h2>Contact Details</h2>
-          <h3>Contact us 7 days a week <br /><br />
-          0408 425 907
-          </h3>
-          <div className='contact-links'>
-          <a href='/' className='footer-link'><img src={FacebookLogo} alt='Facebook Logo' className='contact-logo' /></a>
-          <a href='/' className='footer-link'><img src={InstagramLogo} alt='Instagram Logo' className='contact-logo'/></a>
-          <a href='/' className='footer-link'><FontAwesomeIcon icon={faEnvelope} className='contact-logo'/></a>
-          <a href='/' className='footer-link'><FontAwesomeIcon icon={faPhone} className='contact-logo'/></a>
+        <div className="black-footer-content">
+          <div className="service-areas">
+            <h2>Service Area</h2>
+            <p>Servicing all of Melbourne including Geelong and Torquay</p>
+            <Link to="/" className='footer-link'>
+              <FontAwesomeIcon icon={faHouse} className='home-link'/>
+            </Link>
+          </div>
+          <div className="contact-details">
+            <h2>Contact Details</h2>
+            <h3>Contact us 7 days a week <br /><br />
+            0408 425 907
+            </h3>
+            <div className='contact-links'>
+              <a href='https://facebook.com' className='footer-link' target="_blank" rel="noopener noreferrer">
+                <img src={FacebookLogo} alt='Facebook Logo' className='contact-logo' />
+              </a>
+              <a href='https://instagram.com' className='footer-link' target="_blank" rel="noopener noreferrer">
+                <img src={InstagramLogo} alt='Instagram Logo' className='contact-logo'/>
+              </a>
+              <a href='mailto:your-email@example.com' className='footer-link'>
+                <FontAwesomeIcon icon={faEnvelope} className='contact-logo'/>
+              </a>
+              <a href='tel:+0408425907' className='footer-link'>
+                <FontAwesomeIcon icon={faPhone} className='contact-logo'/>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
       <div className="footer-content">
-        <a href='/'><img src={FooterLogo} alt="Powerful Projects Logo" /></a>
+        <Link to="/">
+          <img src={FooterLogo} alt="Powerful Projects Logo" />
+        </Link>
         <p>&copy; 2024 Powerful Projects | All Rights Reserved</p>
       </div>
     </footer>
--- src/Pages/components/QuoteSection.js
+++ src/Pages/components/QuoteSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import '../../stylesheets/QuoteSection.css';
-import QuoteSectionImage from '../../assets/images/Pitch_AlphingtonDeck_External.jpg'
+import QuoteSectionImage from '../../assets/images/Pitch_AlphingtonDeck_External.jpg';
 
 const QuoteSection = () => {
   return (
@@ -12,7 +13,9 @@ const QuoteSection = () => {
         <h2>WHY US?</h2>
         <h1>We get it done.</h1>
         <p>Every project comes with its own needs and challenges. Tell us your priorities, and we'll fill them to your satisfaction.</p>
-        <button onClick={() => (window.location.href = "/contact-us")}>Get A Free Quote</button>
+        <Link to="/contact-us">
+          <button type="button">Get A Free Quote</button>
+        </Link>
       </div>
     </div>
   );
